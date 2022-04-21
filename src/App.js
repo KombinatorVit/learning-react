8a1fc3dca662ba695b77bbb113bfd38d8237d7aa
Vit Onischenko <slonik0775@gmail.com>
2022-04-21 05:27:22 +0300

    Events in React and recall this

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ constructor(props){
 super(props);
 this.state = {
   years: 27,
-  text: '+++'
+  text: '+++',
+  position: ''
 }
 }
 
@@ -18,16 +19,30 @@ nextYear = () => {
 }))
 }
 
+commitInputChanges = (e, color) => {
+  console.log(color)
+this.setState({
+  position: e.target.value
+})
+}
 
 
   render(){
     const{name, surname, link} = this.props;
+    const { position,text, years} = this.state;
     return (
    <div>
-     <button onClick={this.nextYear}>{this.state.text}</button>
-      <h1>My name is {name}, surname -{surname}, age - { this.state.years} </h1>
+     <button onClick={this.nextYear}>{text}</button>
+      <h1>My name is {name}, surname -{surname}, 
+      age - {years}, 
+      position ={position} </h1>
   
        <a href={link}>My profile</a>
+
+       <form >
+         <span>Введите должность</span>
+         <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+       </form>
     </div>
     )
   }
